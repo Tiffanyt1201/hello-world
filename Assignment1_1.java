7a1e621ba2b52bf004ca8b023968bf2e55770176
Tiffanyt1201 <46972701+Tiffanyt1201@users.noreply.github.com>
2019-03-24 18:52:40 -0600

    added more javadoc

--- Assignment1_1.java
+++ Assignment1_1.java
@@ -1,9 +1,14 @@
 import java.util.Scanner;
 
 /**
- * The {@code Assignment1_1} calss contains a static main method.
+ * THis class takes imput from user in terminal and prints it out.
  */
 public class Assignment1_1 {
+    /**
+     * method uses a scanner to take input from user and prints out a message
+     * 
+     * @param args A String containing the arg input
+     */
     public static void main(String[] args) {
         System.out.println("Hello World"); 
 		Scanner input = new Scanner(System.in);
