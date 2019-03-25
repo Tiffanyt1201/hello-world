fc480a89b32231e5c895d34921d1c31a9c31a67d
Tiffanyt1201 <46972701+Tiffanyt1201@users.noreply.github.com>
2019-03-24 18:23:50 -0600

    Add files via upload

---
+++ Assignment1_1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Assignment1_1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World"); 
+		Scanner input = new Scanner(System.in);
+		String username = input.nextLine(); 
+		System.out.println("Hello " + username); 
+    }
+}
