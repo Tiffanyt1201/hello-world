3a352a5005b6665bbbcaabd56aa8da1a6f24cc63
Tiffanyt1201 <46972701+Tiffanyt1201@users.noreply.github.com>
2019-03-24 18:37:50 -0600

    added javadoc

--- Assignment1_1.java
+++ Assignment1_1.java
@@ -1,5 +1,8 @@
 import java.util.Scanner;
 
+/**
+ * The {@code Assignment1_1} calss contains a static main method.
+ */
 public class Assignment1_1 {
     public static void main(String[] args) {
         System.out.println("Hello World"); 
