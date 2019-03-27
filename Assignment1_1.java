b28e2b1b093ad66c4266932c7ccfffb4cbeadaa8
Tiffanyt1201 <46972701+Tiffanyt1201@users.noreply.github.com>
2019-03-27 02:42:17 -0600

    again

--- Assignment1_1.java
+++ Assignment1_1.java
@@ -2,12 +2,14 @@ import java.util.Scanner;
 
 /**
  * This class takes imput from user in terminal and prints it out.
+ * Add more javadoc add more javadoc
+ * 
  *
  */
 public class Assignment1_1 {
     /**
      * method uses a scanner to take input from user and prints out a message
-     * first it prints hello, then it asks for a user input and then it prints hello with whatever the user inputed. 
+     * first it prints hello, then it asks for a user input and then it prints hello with whatever the user inputed.
      *
      * @param args A String containing the arg input
      */
